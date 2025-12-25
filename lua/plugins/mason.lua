7019fc551ffc07013cde36ca10658e14579c1662
limux1972 <liumengjiang@msn.com>
2025-12-25 16:28:02 +0800

    更新自hx-wsl-debian

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,16 +1,43 @@
 return {
   -- Mason 包管理器
+  --[[
   {
     "williamboman/mason.nvim",
-    config = true, -- 使用完全默认配置
+    init = function()
+      -- 在插件加载前拦截系统调用
+      local original_executable = vim.fn.executable
+      vim.fn.executable = function(cmd)
+        -- mason 检查的这些命令，直接返回存在
+        local mason_checks = {
+          "ruby", "gem", "composer", "php", "npm",
+          "node", "javac", "java", "julia"
+        }
+        
+        for _, check in ipairs(mason_checks) do
+          if cmd == check then
+            return 1 -- 返回 "存在"
+          end
+        end
+        
+        -- 其他命令使用原始检查
+        return original_executable(cmd)
+      end
+    end,
+    config = function()
+      require("mason").setup({
+        ui = {
+          check_outdated_packages_on_open = false,
+        },
+      })
+    end,
   },
-  
+
   -- Mason 与 LSP 配置的桥梁
   {
     "williamboman/mason-lspconfig.nvim",
     config = true, -- 使用完全默认配置
   },
-  
+  --]] 
   -- 自动补全引擎
   {
     "hrsh7th/nvim-cmp",
