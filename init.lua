81dfbd22e82252d09ba0b9ac60741b3b75e10f32
limux <liumengjiang@msn.com>
2026-01-09 15:47:13 +0800

    FB15 本地 nvim 配置

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 -- ~/.config/nvim/init.lua
 -- vim.g.loaded_luasnip = 1
 
+-- 导入模块
+local python_config = require("utils.python")
+
+-- 自动设置 Python 路径
+python_config.setup()
+
 -- 基础设置模块
 require("config.basic")
 
