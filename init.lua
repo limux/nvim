73b4fb4db79287589b26cbb638840b8596fd425a
limux <liumengjiang@msn.com>
2026-01-09 15:05:01 +0800

    FB15 本地 nvim 配置

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
 -- ~/.config/nvim/init.lua
-
-
--- 设置 Neovim 专用 Python 环境
--- vim.g.python3_host_prog = vim.fn.expand('~/heoxsoft/nvim-venv/bin/python')
-
 -- vim.g.loaded_luasnip = 1
 
 -- 基础设置模块
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,22 +1,10 @@
--- gruvbox 颜色主题
--- 必须 return 一个列表！
 return {
-  --[[
-  { import = "plugins.ui" },
   -- 可以继续导入其他分类
-  -- { import = "plugins.tools" },
   -- { import = "plugins.lsp" },
+  { import = "plugins.lualine" },
+  { import = "plugins.dracula" },
   { import = "plugins.startify" },
-  -- { import = "plugins.treesitter" },
-
-  {
-    "ellisonleao/gruvbox.nvim",     -- 插件仓库地址
-    priority = 1000,                -- 高优先级，先加载
-    config = function()             -- 插件加载后的配置函数
-      -- 设置颜色主题
-      -- vim.cmd.colorscheme("gruvbox")
-    end,
-  },
+  { import = "plugins.treesitter" },
 
   {
     "nvim-treesitter/nvim-treesitter",
@@ -31,5 +19,4 @@ return {
       })
     end
   }
-  ]]
 }
